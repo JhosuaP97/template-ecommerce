0e345b20566d15d61eb749d1d8eaf124ef01aba3
jhosua.tsakana <jhosua.tsakana@gmail.com>
2022-08-11 15:52:50 -0500

    add: queries to get data

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+export * as db from "./db";
+export * from "./constants";
+
+// Database test
+export * as seedDataBase from "./products";
---
+++ pages/api/seed.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db, seedDataBase } from "../../database";
+import { Product } from "../../models";
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (process.env.NODE_ENV === "production") {
+    res.status(401).json({ message: "No access to the service" });
+  }
+
+  await db.connect();
+
+  await Product.deleteMany();
+
+  await Product.insertMany(seedDataBase.initialData.products);
+
+  await db.disconnect();
+
+  res.status(200).json({ message: "Process done correctly" });
+}

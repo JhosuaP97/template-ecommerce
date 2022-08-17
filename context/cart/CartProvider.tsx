388612ed6c2e58c18df0302077dc38cd3b738d7d
JhosuaP97 <jhosuapachon@gmail.com>
2022-08-17 14:00:12 -0500

    add: function to add load update remove items from the cart

---
+++ context/cart/CartProvider.tsx
@@ -0,0 +1,130 @@
+import { FC, useEffect, useReducer } from "react";
+import Cookie from "js-cookie";
+import { CartContext, cartReducer } from "./";
+import { ICartProduct } from "../../interfaces/cart";
+
+export interface CartState {
+  cart: ICartProduct[];
+  numberOfItems: number;
+  subTotal: number;
+  tax: number;
+  total: number;
+}
+
+const Cart_INITIAL_STATE: CartState = {
+  cart: [],
+  numberOfItems: 0,
+  subTotal: 0,
+  tax: 0,
+  total: 0,
+};
+
+type CartProviderProps = {
+  children: React.ReactNode;
+};
+
+export const CartProvider: FC<CartProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, Cart_INITIAL_STATE);
+
+  useEffect(() => {
+    try {
+      const cookieProducts = Cookie.get("cart")
+        ? JSON.parse(Cookie.get("cart")!)
+        : [];
+
+      dispatch({
+        type: "Cart - LoadCart from cookies | storage",
+        payload: cookieProducts,
+      });
+    } catch (error) {
+      dispatch({
+        type: "Cart - LoadCart from cookies | storage",
+        payload: [],
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state.cart.length > 0) Cookie.set("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
+  useEffect(() => {
+    const numberOfItems = state.cart.reduce(
+      (prev, current) => current.quantity + prev,
+      0
+    );
+
+    const subTotal = state.cart.reduce(
+      (prev, current) => current.price * current.quantity + prev,
+      0
+    );
+
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+
+    const orderSummary = {
+      numberOfItems,
+      subTotal,
+      tax: subTotal * taxRate,
+      total: subTotal * (taxRate + 1),
+    };
+
+    dispatch({ type: "Cart - Update order summary", payload: orderSummary });
+  }, [state.cart]);
+
+  const addProductToCart = (product: ICartProduct) => {
+    const productInCart = state.cart.some((item) => item._id === product._id);
+    if (!productInCart) {
+      return dispatch({
+        type: "Cart - Update products in cart",
+        payload: [...state.cart, product],
+      });
+    }
+
+    const productInCartButWithDifferentSize = state.cart.some(
+      (item) => item._id === product._id && item.size === product.size
+    );
+    if (!productInCartButWithDifferentSize) {
+      return dispatch({
+        type: "Cart - Update products in cart",
+        payload: [...state.cart, product],
+      });
+    }
+
+    const updatedProductsInCart = state.cart.map((item) => {
+      if (item._id !== product._id) return item;
+      if (item.size !== product.size) return item;
+
+      item.quantity += product.quantity;
+
+      return item;
+    });
+
+    dispatch({
+      type: "Cart - Update products in cart",
+      payload: updatedProductsInCart,
+    });
+  };
+
+  const updateCartProductQuantity = (product: ICartProduct) => {
+    dispatch({ type: "Cart - Change cart quantity", payload: product });
+  };
+
+  const removeCartProduct = (product: ICartProduct) => {
+    dispatch({ type: "Cart - Remove product in cart", payload: product });
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+
+        /* Methods */
+        addProductToCart,
+        updateCartProductQuantity,
+        removeCartProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};

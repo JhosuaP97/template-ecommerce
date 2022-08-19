bb3a56a300b6318a038c18234e03a12a52265da7
JhosuaP97 <jhosuapachon@gmail.com>
2022-08-19 16:03:34 -0500

    create: login

---
+++ pages/api/user/login.ts
@@ -0,0 +1,62 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcryptjs from "bcryptjs";
+import { db } from "../../../database";
+import { User } from "../../../models";
+import { jwt } from "../../../utils";
+
+type Data =
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        name: string;
+        email: string;
+        role: string;
+      };
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "POST":
+      return loginUser(req, res);
+    default:
+      return res.status(400).json({ message: "Bad Request" });
+  }
+}
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { email = "", password = "" } = req.body;
+
+  await db.connect();
+
+  const user = await User.findOne({ email });
+
+  await db.disconnect();
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "User or password is not correct - EMAIL" });
+  }
+
+  if (!bcryptjs.compareSync(password, user.password!)) {
+    return res
+      .status(400)
+      .json({ message: "User or password is not correct - PASSWORD" });
+  }
+
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
+
+  return res.status(200).json({
+    token,
+    user: {
+      email,
+      role,
+      name,
+    },
+  });
+};
